99ecd91add3722825ade44556ba748fa3577ee25
xiorxa <68778263+xiorxa@users.noreply.github.com>
2021-01-17 13:11:27 +0300

    Update ottoman.py

--- ottoman.py
+++ ottoman.py
@@ -12,6 +12,16 @@ async def on_ready():
 @client.command()
 async def deneme(ctx):
     await ctx.send("DENEME")
-
+   
+@client.command()
+async def embed(ctx):
+    embed = discord.Embed(title="DENEME",description="DENEME")
+    embed.add_field(name="DENEME",value="DENEME")
+    embed.add_field(name="DENEME",value="DENEME")
+    embed.set_image(url="[İMAGE URL]")
+    embed.set_thumbnail(url="[İMAGE URL]")
+    embed.set_footer(text="DENEME BOT")
+    await ctx.send(embed=embed)
+  
 token = "[TOKEN]"
 client.run(token)
